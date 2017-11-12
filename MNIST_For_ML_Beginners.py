dc14b08b2f6a982848ce40d56ab6ca7514243548
isudzumi <isudzumi@gmail.com>
2017-11-13 01:52:49 +0900

    Update MNIST4beginner

--- MNIST_For_ML_Beginners.py
+++ MNIST_For_ML_Beginners.py
@@ -1,9 +1,10 @@
 # https://www.tensorflow.org/get_started/mnist/beginners
 
-import input_data
 import tensorflow as tf
+import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = imput_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
@@ -14,9 +15,9 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
-sess.tf.Session()
+sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
